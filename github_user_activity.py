b0bf449e527fd2f159514fbf9317098f6e56d93a
janakchhatkuli <janakchhatkuli1234@gmail.com>
2025-04-29 09:58:48 +0545

    new event updated

--- github_user_activity.py
+++ github_user_activity.py
@@ -18,14 +18,16 @@ def fetch_user_activity(username):
         event_time=event['created_at']
 
         if event_type== "PushEvent":
-            commit_count=len(event['payloads']['commits'])
-            print(f"Pushed {commit_count} commits on {event_repo} at {event_time}")
+            commit_count=len(event['payload']['commits'])
+            print(f"Pushed {commit_count} commit{'s' if commit_count > 1 else ''} to {event_repo} at {event_time}")
         elif event_type == "WatchEvent":
             print(f"Starred on {event_repo}")
         elif event_type=="CommitCommentEvent":
+            action = event['payload']['action']
             print(f"{action} a comment on a commit on {event_repo} at {event_time} ")
         elif event_type == "CreateEvent":
-            print(f"Created a {ref_type} on {ref} at {event_time}")
+            ref_type = event['payload']['ref_type']
+            print(f"Created a {ref_type} on {event_repo} at {event_time}")
         else:
             print(f"{event_type} on {event_repo} at {event_time}")
 
